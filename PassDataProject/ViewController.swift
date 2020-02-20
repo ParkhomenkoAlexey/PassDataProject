d25c57423b62100d6d4de26960b1dd0fe902e60c
ParkhomenkoAlexey <Parkhomenko_AD@mail.ru>
2020-02-20 10:01:26 +0500

    19 kit

--- PassDataProject/ViewController.swift
+++ PassDataProject/ViewController.swift
@@ -17,6 +17,17 @@ class ViewController: UIViewController {
     
     
     @IBAction func loginTapped(_ sender: UIButton) {
+        performSegue(withIdentifier: "detailSegue", sender: nil)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let dvc = segue.destination as? SecondViewController else { return }
+        dvc.login = loginTF.text
+        
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
     }
 }
 
