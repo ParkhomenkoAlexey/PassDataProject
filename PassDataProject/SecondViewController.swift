6496e7c3026eec17753f37bc1345594d8d1b2e6c
ParkhomenkoAlexey <Parkhomenko_AD@mail.ru>
2020-02-20 09:07:11 +0500

    Initial Commit

---
+++ PassDataProject/SecondViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SecondViewController.swift
+//  PassDataProject
+//
+//  Created by Алексей Пархоменко on 20.02.2020.
+//  Copyright © 2020 Алексей Пархоменко. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+    
+    
+    @IBOutlet weak var label: UILabel!
+    
+
+    @IBAction func goBackTapped(_ sender: UIButton) {
+    }
+}
---
+++ PassDataProject/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  PassDataProject
+//
+//  Created by Алексей Пархоменко on 20.02.2020.
+//  Copyright © 2020 Алексей Пархоменко. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var loginTF: UITextField!
+    
+    @IBOutlet weak var passwordTF: UITextField!
+    
+    
+    
+    @IBAction func loginTapped(_ sender: UIButton) {
+    }
+}
+
